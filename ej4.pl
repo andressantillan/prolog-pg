6a4cbe4548efa59085fe68f1791b74cc44bedb31
Andres Santillan <afs9719@gmail.com>
2023-04-01 11:55:52 -0300

    Avance en ejercicio 5

--- ej4.pl
+++ ej4.pl
@@ -3,11 +3,17 @@ Instrucciones: colocar en los cuadros en blanco las cifras (de 0 a 9) correspond
 y horizontalmente, efectuando las operaciones indicadas, den la solución reseñada.
 Para resolver este problema se pide codificar el predicado result(A1,A2,A3,B1,B3,C1,C2,C3), que dé
 el o los resultados factibles para:
-(A1 * A2) * A3 = 6
-(B1 * 5) - B3 = 1
-(C1 - C2) * C3 = 4
-(A1 - B1) * C1 = 7
-(A2 + 5) / C2 = 1
+(A1  A2) x A3 = 6
+(B1 x 5) - B3 = 1
+(C1 - C2) x C3 = 4
+(A1 - B1) x C1 = 7
+(A2 + 5) div C2 = 1
 (A3 + B3) - C3 = 7 **/
 
-result()
+result(A1, A2, A3, B1, B3, C1, C2, C3) :-
+    (B1*5)-B3 is 1,
+    (A1*A2)*A3 is 6,
+    (C1-C2)*C3 is 4,
+    (A1-B1)*C1 is 7,
+    (A2 + 5)/C2 is 1,
+    (A3+B3)-C3 is 7.
--- ej5.pl
+++ ej5.pl
@@ -13,4 +13,14 @@ pertenece(Elemento, [_|Resto]) :- pertenece(Elemento, Resto).
 longitud([], 0).
 longitud([Inicio|Resto], Cantidad) :- longitud(Resto, CantidadRestante), Cantidad is CantidadRestante + 1.
 
-agregar(Elemento, Lista, ListaNueva) :- ListaNueva = [Elemento|Lista].
+% agregar(Elemento, Lista, ListaNueva) :- ListaNueva = [Elemento|Lista].
+agregar(Elemento, Lista, [Elemento|Lista]).
+
+agregar_en_posicion(Elemento, Posicion, [], [Elemento]).  
+
+agregar_en_posicion(Elemento, 1, Lista, [Elemento|Lista]).
+
+agregar_en_posicion(Elemento, Posicion, [_|Resto], ListaNueva) :-
+    agregar_en_posicion(Elemento, PosicionNueva, Resto, ListaNueva),
+    PosicionNueva is Posicion-1.
+    
